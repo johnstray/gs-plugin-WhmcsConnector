7e9bb725e1c6585d8142b8a3dae72cbaf52c75cb
John Stray <johnstray@users.noreply.github.com>
2021-08-25 14:59:36 +0930

    Create en_US.php

---
+++ plugins/gs-whmcs/lang/en_US.php
@@ -0,0 +1,11 @@
+<?php if ( defined('IN_GS') === false ) { die( 'You cannot lod this file directly!' ); }
+/**
+ * Plugin Name: WHMCS Connector
+ * Description: Connects a WHMCS installation to GetSimple to allow the fetching of information.
+ * Version: 1.0.0-alpha
+ * Author: John Stray
+ */ ?>
+
+$i18n = array (
+    'KEY' => 'Translated Value',
+);
