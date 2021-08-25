a38bc31a9a6e43e4c1067d5e3535f50c405ca7cd
John Stray <johnstray@users.noreply.github.com>
2021-08-25 14:59:53 +0930

    Update en_US.php

--- plugins/gs-whmcs/lang/en_US.php
+++ plugins/gs-whmcs/lang/en_US.php
@@ -4,7 +4,7 @@
  * Description: Connects a WHMCS installation to GetSimple to allow the fetching of information.
  * Version: 1.0.0-alpha
  * Author: John Stray
- */ ?>
+ */
 
 $i18n = array (
     'KEY' => 'Translated Value',
