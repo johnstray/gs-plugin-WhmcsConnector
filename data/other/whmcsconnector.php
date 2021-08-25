59309b5383e09c6d9959c15328b686244aacff61
John Stray <johnstray@users.noreply.github.com>
2021-08-25 15:03:17 +0930

    Create whmcsconnector.php

---
+++ data/other/whmcsconnector.php
@@ -0,0 +1,6 @@
+<?xml version="1.0"?>
+<settings>
+    <apikey><![CDATA[string]]></apikey>
+    <apisecret><![CDATA[string]]></apisecret>
+    <apiurl><![CDATA[https://example.com/]]></apiurl>
+</settings>
