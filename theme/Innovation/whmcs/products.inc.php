bd58e4b08bedfe9af4ed6846737f9b23b3ec64cb
John Stray <johnstray@users.noreply.github.com>
2021-08-25 15:15:33 +0930

    Create products.inc.php

---
+++ theme/Innovation/whmcs/products.inc.php
@@ -0,0 +1,69 @@
+<?php if ( defined('IN_GS') === false ) { die( 'You cannot lod this file directly!' ); }
+/**
+ * Plugin Name: WHMCS Connector
+ * Description: Connects a WHMCS installation to GetSimple to allow the fetching of information.
+ * Version: 1.0.0-alpha
+ * Author: John Stray
+ */
+
+//@TODO: Refactor this for the default Innovation theme
+
+$getProducts = gs_whmcs_api( 'GetProducts', ['gid'=>'1'] );
+$products = $getProducts['products']['product'];
+ob_start(); ?>
+
+<div class="plan-navtab">
+
+    <div class="nav-wrapper d-flex justify-content-center mb-70" data-animate="fadeInUp" data-delay=".3">
+		    <ul class="nav nav-tabs justify-content-center" role="tablist">
+			      <li class="nav-item">
+				        <a class="active" data-toggle="tab" href="#monthly" role="tab">Monthly</a>
+		    	  </li>
+    			  <li class="nav-item">
+				        <a data-toggle="tab" href="#quarterly" role="tab">Quarterly</a>
+			      </li>
+            <li class="nav-item">
+				        <a data-toggle="tab" href="#yearly" role="tab">Yearly</a>
+			      </li>
+		    </ul>
+    </div>
+
+    <div class="tab-content">
+      
+        <div id="monthly" class="tab-pane fade show active" role="tabpanel">
+            <div class="product-slider">
+
+                <?php foreach ( $products as $plan ) {
+                    $freq = 'monthly';
+                    include(__DIR__.'/includes/single-plan.php');
+                } ?>
+                
+            </div>
+        </div>
+      
+        <div id="quarterly" class="tab-pane fade" role="tabpanel">
+            <div class="product-slider">
+
+                <?php foreach ( $products as $plan ) {
+                    $freq = 'quarterly';
+                    include(__DIR__.'/includes/single-plan.php');
+                } ?>
+                
+            </div>
+        </div>
+      
+        <div id="yearly" class="tab-pane fade" role="tabpanel">
+            <div class="product-slider">
+
+                <?php foreach ( $products as $plan ) {
+                    $freq = 'yearly';
+                    include(__DIR__.'/includes/single-plan.php');
+                } ?>
+                
+            </div>
+        </div>
+      
+    </div>
+</div>
+
+<?php $replace = ob_get_clean(); ?>
